ad0af0b4acce30d92279b5d508942ad1c6edab34
Jose Esteves <jose@ceibo.co>
2020-07-15 11:07:04 -0300

    id_server strategy - beta working version

---
+++ lib/ueberauth_id_server.ex
@@ -0,0 +1,18 @@
+defmodule UeberauthIdServer do
+  @moduledoc """
+  Documentation for UeberauthIdServer.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UeberauthIdServer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ueberauth_id_server_test.exs
@@ -0,0 +1,8 @@
+defmodule UeberauthIdServerTest do
+  use ExUnit.Case
+  doctest UeberauthIdServer
+
+  test "greets the world" do
+    assert UeberauthIdServer.hello() == :world
+  end
+end
